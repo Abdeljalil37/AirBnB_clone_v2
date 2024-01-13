6ff72086f707778ffa78c29f9047d7d9fa548c43
Abdeljalil37 <widadpop@gmail.com>
2024-01-13 20:38:47 +0100

    chmod file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-sudo apt-get update
-sudo apt-get install -y nginx
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-echo "It works" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
-
+# Setup a web servers for the deployment of web_static.
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-
 sudo service nginx restart
