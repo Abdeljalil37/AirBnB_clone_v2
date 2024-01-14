89fc494b7d6b227c1f63e4f0452ca8789f797541
Abdeljalil37 <widadpop@gmail.com>
2024-01-14 23:00:01 +0100

    last try

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Fabfile to delete out-of-date archives. """
 from fabric.api import *
+import os
 
 env.hosts = ["52.201.190.48", "54.90.56.0"]
 
@@ -17,7 +18,11 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
