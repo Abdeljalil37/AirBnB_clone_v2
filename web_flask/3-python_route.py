84b7e33b8a6d7eac1ee133fefc8c2ebd766f1067
Abdeljalil37 <widadpop@gmail.com>
2024-01-28 22:09:27 +0100

    task 3-python_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+from urllib.parse import unquote
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """ print hello hbnb """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ print hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<path:text>')
+def c_route(text):
+    """ print c followed by the given text """
+    value = unquote(text.replace('_', ' '))
+    return 'C {}'.format(value)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_route(text='is cool'):
+    """ print python followed by the given text
+    with default value of text: is cool """
+    value = unquote(text.replace('_', ' '))
+    return 'python {}'.format(value)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
