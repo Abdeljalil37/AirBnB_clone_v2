68e80fff0c561e2f3054b965091ec72ee6581847
Abdeljalil37 <widadpop@gmail.com>
2024-01-29 02:43:36 +0100

    task 6 odd and even numbers

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,1 +1,75 @@
 #!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """ print hello hbnb """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ print hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<path:text>')
+def c_route(text):
+    """ print c followed by the given text """
+    value = text.replace('_', ' ')
+    return 'C {}'.format(value)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_route(text='is cool'):
+    """ print python followed by the given text
+    with default value of text: 'is cool' """
+    value = text.replace('_', ' ')
+    return 'Python {}'.format(value)
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    """ print the given number followed by 'is a number' """
+    return '{} is a number'.format(int(n))
+
+
+@app.route('/number_template/<int:n>')
+def number_template_route(n):
+    """ print the given number followed by 'is a number' """
+    return render_template('5-number.html', number=int(n))
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """ print the given number followed by 'is a number' """
+    if n % 2 == 0:
+        value = 'even'
+    else:
+        value = 'odd'
+    return render_template(
+        '6-number_odd_or_even.html',
+        number=int(n),
+        value=value)
+
+
+@app.route('/states_list')
+def states_list():
+    """ print the states """
+    from models import storage
+    from models.state import State
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+    return render_template('5-number.html', number=int(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
